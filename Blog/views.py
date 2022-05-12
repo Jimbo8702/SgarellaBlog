9bbf43fd32ff43b2cb8ff40907f2e8d9b98d6e91
James Sgarella <Jimbo8702@gmail.com>
2022-05-12 18:37:44 -0400

    blog models

--- Blog/views.py
+++ Blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from .models import *
+from django.contrib.auth.decorators import login_required
+from .forms import ProfileForm, BlogPostForm
+from django.views.generic import UpdateView
+from django.contrib import messages
 # Create your views here.
-def home(request):
-    return render(request)
+def blogs(request):
+    pass
