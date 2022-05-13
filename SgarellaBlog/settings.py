0236befd70585c3e7d30ba79eb956f142dcc3b0e
James Sgarella <Jimbo8702@gmail.com>
2022-05-13 07:45:43 -0400

    env file added

--- SgarellaBlog/settings.py
+++ SgarellaBlog/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9lrndtv4+jsoeyxa8yd=!tl9@5ic%62k%8+lri!hyz^pdvz_5l"
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
